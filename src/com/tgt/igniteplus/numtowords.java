ac30349268c159221c2674dd5750048b2fbe64eb
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-03-17 23:03:17 +0530

    done

---
+++ src/com/tgt/igniteplus/numtowords.java
@@ -0,0 +1,41 @@
+package com.tgt.igniteplus;
+
+import java.util.Scanner;
+
+public class numtowords {
+    public static void main(String[] args) {
+        int n,length;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter any number");
+        n=in.nextInt();
+        System.out.println("Enter length of number");
+        length=in.nextInt();
+        /*for(int i=0;i<length;i++)
+        {
+            switch(n[i])
+            {
+                case 1:System.out.println("One \t");
+                       break;
+                case 2:System.out.println("Two\t");
+                    break;
+                case 3:System.out.println("Three \t");
+                    break;
+                case 4:System.out.println("Four\t");
+                    break;
+                case 5:System.out.println("Five \t");
+                    break;
+                case 6:System.out.println("Six\t");
+                    break;
+                case 7:System.out.println("Seven \t");
+                    break;
+                case 8:System.out.println("Eight \t");
+                    break;
+                case 9:System.out.println("Nine \t");
+                    break;
+                case 0:System.out.println("Zero \t");
+                    break;
+                default:
+            }
+        }*/
+    }
+}
---
+++ src/com/tgt/igniteplus/series2.java
@@ -0,0 +1,18 @@
+package com.tgt.igniteplus;
+
+import java.util.Scanner;
+
+public class series2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+                System.out.print(i+  "\t");
+            else
+                System.out.print("-" + i + "\t");
+        }
+    }
+}
