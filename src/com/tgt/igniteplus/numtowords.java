82e7894176771394b710297a0c1532c8facf0f1b
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-03-21 13:42:13 +0530

    done

--- src/com/tgt/igniteplus/numtowords.java
+++ src/com/tgt/igniteplus/numtowords.java
@@ -2,55 +2,6 @@ package com.tgt.igniteplus;
 
 import java.util.Scanner;
 
-/*public class numtowords {
-    public static void main(String[] args) {
-        int n,length;
-        int r[] = new int[20];
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter any number");
-        n=in.nextInt();
-        System.out.println("The number in words is");
-        int r=0,temp=n,dig;
-        while(n!=0)
-        {
-            for(int i=0;)
-            r=n%10;
-            n=n/10;
-        }
-        temp = r;
-        while (temp != 0)
-        {
-            dig = temp % 10;
-            switch(dig)
-            {
-                case 1:System.out.print("One \t");
-                       break;
-                case 2:System.out.print("Two \t");
-                    break;
-                case 3:System.out.print("Three \t");
-                    break;
-                case 4:System.out.print("Four\t");
-                    break;
-                case 5:System.out.print("Five \t");
-                    break;
-                case 6:System.out.print("Six\t");
-                    break;
-                case 7:System.out.print("Seven \t");
-                    break;
-                case 8:System.out.print("Eight \t");
-                    break;
-                case 9:System.out.print("Nine \t");
-                    break;
-                case 0:System.out.print("Zero \t");
-                    break;
-                default:
-            }
-        }
-
-    }
-}
-*/
-
 public class numtowords
 {
     public static void main(String[] args) {
